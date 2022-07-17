c63efeab8741e674eb04400dc2292d35a771d4f4
MsrProgrammer <mohammadsaeedi7798@yahoo.com>
2022-07-18 00:56:26 +0430

    redesign whole UI v.1.0.1

--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -3,11 +3,11 @@ import TypeAnimation from 'react-type-animation';
 const Welcome = () => {
   const h1="Hi, I'm mohammad"
   return ( 
-    <div className="welcome welcome-container bg-purple-900 relative">
-      <Image className="opacity-[79%]" src="/welcome.jpg" alt="mouse scroll" layout="fill"
+    <div className="welcome welcome-container  relative">
+      <Image src="/background.svg" alt="mouse scroll" layout="fill"
         objectFit="cover"
         quality={100} />
-      <div className="absolute bottom-8 z-10 animate-bounce cursor-pointer block lg:hidden "> <a href="#about"><Image src="/scroll.svg" alt="touch scroll" width={80} height={80} /></a></div>
+      <div className="absolute bottom-8 z-10 animate-bounce cursor-pointer block lg:hidden "> <a href="#about"><Image src="/arrow.svg" alt="touch scroll" width={80} height={80} /></a></div>
       <div className="absolute bottom-8 z-10 animate-bounce cursor-pointer hidden lg:block "> <a href="#about"><Image src="/mouse-cursor.png" alt="mouse scroll" width={80} height={80} /></a></div>
       <div className="z-10">
           <div className="text-white  w-fit flex justify-center"  >    
@@ -15,7 +15,7 @@ const Welcome = () => {
 
            <div className="flex flex-row text-white text-md sm:text-2xl font-mono" >
            <p className="mr-1" >I'm a </p>
-           <div className="w-fit text-sky-900  font-semibold">
+           <div className="w-fit text-yellow-500  font-semibold">
            <TypeAnimation
             cursor={true}
             sequence={[

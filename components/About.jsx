96c6ef7084806339aa5ece59eb4d1e44380de9ec
MsrProgrammer <mohammadsaeedi7798@yahoo.com>
2022-09-12 22:05:52 +0430

    about section redesign v.1.0.1

--- components/About.jsx
+++ components/About.jsx
@@ -3,12 +3,13 @@ const About = () => {
   return (
     <>  
       <div id='about' className="sm:container about my-20 mx-auto p-4 md:p-0 rounded relative"> 
-      <div className="absolute w-5 h-5 bg-[#031019] rounded-full bottom-4 right-4" ></div> 
       <div  className=" p-2 flex flex-wrap w-full lg:w-4/5 mx-auto"> 
-        <div className="bg-cover  opacity-90 bg-bottom w-full md:w-1/3 h-96 md:h-90 rounded  relative" style={{backgroundImage:"url('mohammadsaeedirad.jpg')",backgroundPosition:"center center"}}>
+        <div className="bg-cover  opacity-90 bg-bottom w-full md:w-1/3 h-96 md:h-90 overflow-hidden bg-[#3b6aa038] rounded  relative" style={{backgroundImage:"url('mohammadsaeedirad.png')",backgroundPosition:"center center"}}>
+        {/* <div className="absolute w-40 h-40 bg-[#24628f50] rounded -top-16 -left-16" ></div>  */}
         </div>
         <div className="about-card-wrapper h-full w-full md:w-2/3">
           <div className="h-full mx-auto px-6 md:px-0 md:pt-6 md:-ml-6 relative">
+          <div className="hidden  md:block absolute z-50 w-28 h-28 lg:w-28 lg:h-28 bg-[#febe3596] rounded md:bottom-2 md:right-12 lg:-top-12 lg:-left-12" ></div> 
             <div className="about-card lg:h-full p-6 -mt-6 md:mt-0 relative mb-4 md:mb-0 flex flex-wrap md:flex-wrap items-center rounded">
               <div className="w-full lg:w-1/5 lg:border-right lg:border-solid text-center md:text-left">
                 <h3>Mohammad Saeedi rad</h3>
